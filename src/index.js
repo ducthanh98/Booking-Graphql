e4ead0051f2dbf7bea2655b7969737bd9070c2aa
ThanhLD <thanhld08@gmail.com>
2019-12-05 16:18:26 +0700

    add model

--- src/index.js
+++ src/index.js
@@ -1,20 +1,89 @@
 // @flow
 import express, { Application } from 'express';
+import { buildSchema } from 'graphql';
+import graphqlHttp from 'express-graphql';
+import mongoose from 'mongoose';
+import eventSchema from './models/event.model';
 
 class Server {
-    constructor () {
+    constructor() {
         this.app = express();
+        this.events = [];
         this.port = process.env.port || 3000;
         this.routes();
         this.config();
+        this.useGraphql();
     }
 
     config: void = () => {
         this.app.use(express.json());
-
+        mongoose.connect(`mongodb+srv://admin:JNfcK2lBBh0b9xwm@cluster0-7u2rc.mongodb.net/graphql?retryWrites=true&w=majority`, { useNewUrlParser: true, useUnifiedTopology: true }, (err) => {
+            if (err) console.log(err);
+        })
         this.app.listen(this.port, () => {
             console.log('App is working now');
         });
+    }
+
+    useGraphql = () => {
+        this.app.use('/graphql',
+            graphqlHttp({
+                schema: buildSchema(`
+                type Event {
+                    _id:ID!
+                    title:String!
+                    description:String!
+                    price:Float!
+                    date:String!
+                }
+
+                input EventInput{
+                    title:String!
+                    description:String!
+                    price:Float!
+                    date: String!
+                }
+
+                type RootQuery{
+                    events : [Event!]!
+                }   
+                
+                type RootMutation {
+                    createEvent(eventInput:EventInput):Event
+                }
+            
+                schema {
+                        query: RootQuery
+                        mutation: RootMutation
+                    }
+            `),
+                rootValue: {
+                    events: () => {
+                        return eventSchema.find()
+                            .then((result) => {
+                                return result.map((event) => {
+                                    return { ...event._doc, _id: event._doc._id.toString() }
+                                })
+                            })
+                    },
+                    createEvent: (args) => {
+                        const event = new eventSchema({
+                            title: args.eventInput.title,
+                            description: args.eventInput.description,
+                            price: args.eventInput.price,
+                            date: new Date(args.eventInput.date)
+                        })
+                        event.save()
+                            .then((result) => {
+                                return { ...result._doc, _id: result._doc._id.toString() }
+                            }).catch((err) => {
+                                throw err;
+                            })
+                        return event;
+                    }
+                },
+                graphiql: true
+            }))
     }
 
     routes: void = () => {
---
+++ src/models/event.model.js
@@ -0,0 +1,22 @@
+import { Schema, model } from 'mongoose';
+
+const eventSchema = new Schema({
+    title: {
+        type: String,
+        required: true
+    },
+    description: {
+        type: String,
+        required: true
+    },
+    price: {
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        required: true
+    },
+});
+
+export default model('event', eventSchema);
